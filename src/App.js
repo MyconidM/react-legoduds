a11761642ef90d488da5fa46cd08e8263b7ba57c
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-02-03 12:01:04 +0100

    make legoduds with react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import './css/main.css'
+import Product from './components/Product';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='header'>
+
+      </header>
+      <main className='content'> 
+        <Product/>
+    </main> 
+   </div>
+  );
+}
+
+export default App;
